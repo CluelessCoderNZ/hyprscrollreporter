ab2560b78927cd336a64c6a0ea7ec983a139e4a5
Connor Macdonald <kiwiconnor@proton.me>
2024-02-25 15:05:32 +1300

    Added IPC Event 'workspaceoffset' on hypr render if monitor workspace transition has changed

---
+++ README.md
@@ -0,0 +1,22 @@
+# Hyprscrollreporter
+
++ Simple utility plugin extends hyprland to output its current workspace animation offset via IPC. Animation offsets are
+the sliding annimation you see when transitioning between workspaces, also for touchpad gestures. 
++ By exporting this value you can allow other utilities to track alongside the animation in time. In particular a scrolling wallpaper I am developing.
+
+## Event `workspaceoffset`
+Event data members in order:
++ `monitor_name`: Monitor name of workspace host.
++ `monitor_id`: Monitor id workspace host.
++ `active_workspace`: Id of active workspace.
++ `previous_workspace`: Id of previous workspace.
++ `unit_offset_x`: [-1, +1] value of transition across monitor width.
++ `unit_offset_y`: [-1, +1] value of transition across monitor height. For vertical workspace setups.
+
+**Example IPC Output:**
+```
+workspace>>2
+workspaceoffset>>WL-1,0,2,1,1.000000,0.000000
+workspaceoffset>>WL-1,0,2,1,0.825364,0.000000
+workspaceoffset>>WL-1,0,2,1,0.552560,0.000000
+```
--- main.cpp
+++ main.cpp
@@ -1,15 +1,95 @@
+#define WLR_USE_UNSTABLE
+
+#include <unistd.h>
+#include <vector>
+#include <iostream>
+#include <map>
+
+#include <hyprland/src/includes.hpp>
+#include <any>
+
+#define private public
+#include <hyprland/src/Compositor.hpp>
+#include <hyprland/src/managers/EventManager.hpp>
+#include <hyprland/src/helpers/Monitor.hpp>
+#include <hyprland/src/render/Renderer.hpp>
+#undef private
+
 #include <hyprland/src/plugins/PluginAPI.hpp>
 
 inline HANDLE PHANDLE = nullptr;
+
 
 APICALL EXPORT std::string PLUGIN_API_VERSION() {
     return HYPRLAND_API_VERSION;
 }
 
+struct MonitorTransition {
+    int previous_workspace = -1;
+    int active_workspace = -1;
+    Vector2D unit_offset;
+};
+
+std::map<uint64_t, MonitorTransition> g_lastTransition;
+
+bool transitionEqual(MonitorTransition* a, MonitorTransition* b) {
+    return a->previous_workspace == b->previous_workspace 
+        && a->active_workspace == b->active_workspace
+        && a->unit_offset.x == b->unit_offset.x
+        && b->unit_offset.y == b->unit_offset.y;
+}
+
+bool getCurrentTransition(std::shared_ptr<CMonitor> monitor, MonitorTransition* transition) {
+    CWorkspace* active = g_pCompositor->getWorkspaceByID(monitor->activeWorkspace);
+    if (active == nullptr) return false;
+    
+    Vector2D offset = active->m_vRenderOffset.vec();
+
+    transition->previous_workspace = active->m_sPrevWorkspace.iID;
+    transition->active_workspace = monitor->activeWorkspace;
+    transition->unit_offset.x = offset.x / monitor->vecPixelSize.x;
+    transition->unit_offset.y = offset.y / monitor->vecPixelSize.y;
+
+    return true;
+}
+
+void onNewTransitionValue(std::shared_ptr<CMonitor> monitor, MonitorTransition* transition) {
+    SHyprIPCEvent event;
+    event.event = "workspaceoffset";
+    event.data = std::format("{},{},{},{},{:.6f},{:.6f}",
+        monitor->szName,
+        monitor->ID,
+        transition->active_workspace,
+        transition->previous_workspace,
+        transition->unit_offset.x,
+        transition->unit_offset.y
+    );
+    
+    g_pEventManager->postEvent(event);
+}
+
+void onRenderStage(eRenderStage stage) {
+    if (stage != RENDER_PRE_WINDOWS)
+        return;
+
+    
+    for (auto& monitor : g_pCompositor->m_vMonitors) {
+        MonitorTransition transition;
+        if (!getCurrentTransition(monitor, &transition)) continue;
+
+        if(!g_lastTransition.contains(monitor->ID) 
+            || !transitionEqual(&g_lastTransition[monitor->ID], &transition)) {
+            g_lastTransition[monitor->ID] = transition;
+            onNewTransitionValue(monitor, &transition);
+        }
+        
+    }
+}
+
 APICALL EXPORT PLUGIN_DESCRIPTION_INFO PLUGIN_INIT(HANDLE handle) {
     PHANDLE = handle;
 
-    // ...
+    HyprlandAPI::registerCallbackDynamic(PHANDLE, "render", [&](void* self, SCallbackInfo& info, std::any data) { onRenderStage(std::any_cast<eRenderStage>(data)); });
 
     return {
         "HyprScrollReporter", 
@@ -20,5 +100,5 @@ APICALL EXPORT PLUGIN_DESCRIPTION_INFO PLUGIN_INIT(HANDLE handle) {
 }
 
 APICALL EXPORT void PLUGIN_EXIT() {
-    // ...
+    // Nothing to do
 }
